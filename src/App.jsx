e2a8dc3dae41c81141e694489e3558ec58595e0b
Paul Jabs <p.jabs2@newcastle.ac.uk>
2024-10-03 21:22:31 +0100

    initial setup of react.js project, header and hero section.  Added tailwind css.
    completion of feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./sections/Header.tsx";
 import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
 
 const App = () => {
   return (
     <main>
       <Header></Header>
       <Hero></Hero>
+      <Features></Features>
     </main>
   );
 };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,11 @@
-import { Element } from "react-scroll";
+import { Element, Link as LinkScroll } from "react-scroll";
+import Button from "../components/Button.jsx";
 
 const Hero = () => {
   return (
     <section
       className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36
-    max-md:pt-36 max-md:pb-32 border-2 border-amber-400"
+    max-md:pt-36 max-md:pb-32"
     >
       <Element name="hero">
         <div className="container">
@@ -12,7 +13,27 @@ const Hero = () => {
             <div className="caption small-2 uppercase text-p3">
               Video Editing
             </div>
-            <h1 className="mb-6 h1"></h1>
+            <h1
+              className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2
+             max-md:mb-4 max-md:text-5xl max-md:leading-12"
+            >
+              Amazingly simple
+            </h1>
+            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+              We designed XORA AI Video Editor to be an easy to use, quick to
+              learn, and surprisingly powerful
+            </p>
+            <LinkScroll to="features" offset={-100} spy smooth>
+              <Button icon="/images/zap.svg">Try it now</Button>
+            </LinkScroll>
+          </div>
+
+          <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
+            <img
+              src="/images/hero.png"
+              alt="hero"
+              className="size-1230 max-lg:h-auto"
+            />
           </div>
         </div>
       </Element>
